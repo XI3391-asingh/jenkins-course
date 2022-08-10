3464e7076b791bea9413687f3760cc72109501ec
Abhishek Singh <99200706+XI3391-asingh@users.noreply.github.com>
2022-08-10 16:31:36 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,9 +1,9 @@
 job('NodeJS Docker example') {
-    scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('XI3391-asingh')
-            node / gitConfigEmail('abhishek.singh5@xebia.com')
-        }
+//     scm {
+//         git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+//             node / gitConfigName('XI3391-asingh')
+//             node / gitConfigEmail('abhishek.singh5@xebia.com')
+//         }
     steps {
       dockerBuildAndPublish {
             repositoryName('carbonaraaah/jenkins-jobdsl')
@@ -12,8 +12,6 @@ job('NodeJS Docker example') {
             forcePull(false)
             createFingerprints(false)
             skipDecorate()
-            }
-        }
-    }
+     }  
+  }
 }
-    
