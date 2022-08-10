2b4a09e0f750dd0c90e6523a04d6ffd82487e8de
Abhishek Singh <99200706+XI3391-asingh@users.noreply.github.com>
2022-08-10 12:10:49 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -5,9 +5,9 @@ job('NodeJS Docker example') {
             node / gitConfigEmail('abhishek.singh5@xebia.com')
         }
     }
-    triggers {
-        scm('H/5 * * * *')
-    }
+//     triggers {
+//         scm('H/5 * * * *')
+//     }
     wrappers {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
