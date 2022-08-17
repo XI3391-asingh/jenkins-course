74fd20e542980ff8e2cfa62cc33d87f3040ede69
Abhishek Singh <99200706+XI3391-asingh@users.noreply.github.com>
2022-08-17 12:45:48 +0530

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,24 +1,25 @@
-job('DockerBuildandPublishDSL') {
-    description('Build and Publish Tomcat server Image to docker repository')
-     scm {
-        git {
-        remote {
-                url('https://github.com/XI3391-asingh/Docker-Tomcat.git')
-            }
-            wrappers {
-                preBuildCleanup()
-            } 
-            branch('*/' + 'master')
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
-
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
     steps {
         dockerBuildAndPublish {
             repositoryName('carbonaraaah/jenkins-jobdsl')
             tag('0.1.0')
             registryCredentials('docker-hub')
-            forcePull(true)
-            createFingerprints(true)
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
             skipDecorate()
         }
     }
